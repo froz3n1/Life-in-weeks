db797cde398b48c2aa36cbd088f3629f165b2e34
froz3n1 <76907880+froz3n1@users.noreply.github.com>
2022-03-31 08:37:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+age =int( input("What is your current age?\n"))
+age_left=90-age
+days_left=age_left*365
+weeks_left=age_left*52
+months_left=age_left*12
+result=print(f"You have {days_left} days, {weeks_left} weeks and {months_left} months left.")
+
+
+
